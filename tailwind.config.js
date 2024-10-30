bace62f9b1bae932c9a522090f031111e6dc1a8e
Maxime <lust.maxime@outlook.fr>
2024-10-30 14:25:47 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': "#090F1E",
+        'offWhite': '##FFFBEE',
+        'green': '#08BC80',
+        'blue': '#7482E8',
+        'lightBlue': '#E7ECFF',
+        'red': '#FE0100',
+        'yellow': '#F9D523'
+      },
+      // fontFamily: {
+      //   title: ["compacta", ...defaultTheme.fontFamily.sans],
+      //   body: ["suisse", ...defaultTheme.fontFamily.sans]
+      // },
+      screens: {
+        "4xs": "320px",
+        "3xs": "380px",
+        "2xs": "430px",
+        "xs": "530px"
+      },
+      fontSize: {
+        "2xs": ["0.625rem", "0.875rem"],
+        "3.5xl": ["2rem", "2.5rem"],
+        "4.5xl": ["2.5rem", "2.813rem"],
+        "5.5xl": ["3.375rem", "3.375rem"],
+        "6.5xl": ["4rem", "3.4rem"],
+        "8.5xl": ["7rem", "7.313rem"]
+      },
+      maxWidth: {
+        'screen': '100vw',
+      }
+    }
   },
   plugins: [],
 }
