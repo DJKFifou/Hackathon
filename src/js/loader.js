7bb7294a96f669df59eef33470ed166ef67ac8f3
Maxime <lust.maxime@outlook.fr>
2024-10-31 09:33:15 +0100

    update reviews block

--- src/js/loader.js
+++ src/js/loader.js
@@ -6,9 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
 
     video.addEventListener("ended", function () {
-        loader.classList.add("opacity-0");
-        main.classList.remove("opacity-0");
-        main.classList.add("opacity-100");
+        loader.classList.add("!opacity-0");
+        main.classList.add("!opacity-100");
         body.classList.remove("overflow-hidden");
     });
 });
